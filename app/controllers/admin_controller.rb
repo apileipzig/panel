62e83d3c36d2e19bdcf4ae698ae48921ca4748de
Sven Windisch <sven.windisch@gmail.com>
2011-01-10 16:21:43 +0100

    User activation by admin implemented

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,14 @@
 class AdminController < ApplicationController
-  
+  before_filter :require_admin
+    
   def index
     @inactive_users = User.find(:all, :conditions => [ "active = ?", false ])
+  end
+  
+  def user_activation
+    params[:activation].each do |login,form_values|
+      user = User.find_by_login(login)
+      user.update_attributes(form_values)
+    end
   end
 end
