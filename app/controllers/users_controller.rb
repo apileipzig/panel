77cdcf3e45d041e98cc946e9758ccd077add58c7
Sven Windisch <sven.windisch@gmail.com>
2011-01-17 17:04:31 +0100

    Panel ready for styling. Commiting way to much files now, later cleaning required.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,15 @@ class UsersController < ApplicationController
 
   def show
     @user = @current_user
+    @permissions = Hash.new
+    Permission.all_tables.each do |tablename|
+      @permissions[tablename] = {'read', 'write'}
+      @permissions[tablename]['read'] = @user.permissions.select{|p| p.access == 'read' && p.table == tablename}
+      @permissions[tablename]['write'] = @user.permissions.select{|p| p.access == 'write' && p.table == tablename}
+      
+      # Prevent the display of a table if the user has no single permission on it
+      @permissions.delete(tablename) if @permissions[tablename]['read'].blank? && @permissions[tablename]['write'].blank?
+    end
   end
 
   def edit
@@ -28,6 +37,7 @@ class UsersController < ApplicationController
 
   def update
     @user = @current_user # makes our views "cleaner" and more consistent
+    params[:user].delete('email') # make sure, the user cannot change its unique login value
     if @user.update_attributes(params[:user])
       flash[:notice] = "Account updated!"
       redirect_to account_url
