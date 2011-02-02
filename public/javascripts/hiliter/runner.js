de6afa589389d61f7d7862feccc9ddda5a20566c
Sven Windisch <sven.windisch@gmail.com>
2011-02-02 11:46:11 +0100

    Big cleanup + fixed issue 4

---
+++ public/javascripts/hiliter/runner.js
@@ -0,0 +1,13 @@
+    
+	$(function() 
+    {
+	    $.chili.loadStylesheetInline('.chili-ln-off {list-style-type: none;}');
+	    $.extend($.chili, $.chili.options);
+        
+        if ($.chili.automatic.active) 
+        {
+            $($.chili.automatic.selector, $.chili.automatic.context).chili();
+        }
+    });
+	
+	
