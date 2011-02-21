8db177ae355a2551f5192a09c86d2e3e24a2add9
ben <ben@nerdlabor.de>
2011-02-21 16:01:22 +0100

    moved migrations to api repo

--- db/migrate/20110205165712_add_source_column_to_permissions_table.rb
+++
@@ -1,9 +0,0 @@
-class AddSourceColumnToPermissionsTable < ActiveRecord::Migration
-  def self.up
-    add_column :permissions, :source, :string, :after => :access
-  end
-
-  def self.down
-    remove_column :permissions, :source
-  end
-end
