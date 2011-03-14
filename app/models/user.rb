c5dff32286d7a5d225e6ab102f95ab7253bf6d59
Sven Windisch <sven.windisch@gmail.com>
2011-03-14 22:00:45 +0100

    Added event form and listing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   acts_as_authentic
   has_and_belongs_to_many :permissions
+
+  def may_edit_calendar?
+    !calendar_permissions.blank?
+  end
+  
+  def calendar_permissions
+    permissions.select{|p| p.source == 'calendar' && p.access == 'create'}
+  end
 end
