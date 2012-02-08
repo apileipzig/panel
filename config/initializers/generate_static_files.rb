34b4785d9144796084cace50a7cbca33cd9a9f76
ben <ben@nerdlabor.de>
2012-02-08 23:57:52 +0100

    super weird haml bug

--- config/initializers/generate_static_files.rb
+++ config/initializers/generate_static_files.rb
@@ -5,10 +5,12 @@ marker_header = '<!-- /#Start_Content_Marker - DO NOT (RE)MOVE! -->'
 marker_footer = '<!-- /#End_Content_Marker - DO NOT (RE)MOVE! -->'
 
 begin
+  puts "Generating static files..."
+
 	html = File.open(Rails.root + '../../index.html', 'r') { |f| f.read }
 
   doc = Nokogiri::HTML(html[0,html.index(marker_header)])
-
+  
   #give body a class
   doc.search('body').each do |e|
     e['class'] = "panel"
@@ -31,11 +33,11 @@ begin
 
   header.gsub!('<a class="LoginLink span-1 last" id="login" href="/panel/login" title="Anmelden">Anmelden</a>',login_switch)
   
-  write_to = File.new(Rails.root + "app/views/layouts/_header.html.haml", "w+")
-	write_to.puts(header[0,header.length-16]) #extreme ugly because nokogiri add </body></html> at the end of its document
+  write_to = File.new(Rails.root + "app/views/layouts/header.html.haml", "w+")
+  write_to.write(header[0,header.length-16]) #extreme ugly because nokogiri add </body></html> at the end of its document
 
 	write_to = File.new(Rails.root + "app/views/layouts/footer.html", "w+")
-	write_to.puts(html[html.index(marker_footer)+marker_footer.length,html.length])
+	write_to.write(html[html.index(marker_footer)+marker_footer.length,html.length])
 
   #copy fresh static files from the frontend on every startup only on dev mode
   if Rails.env == 'development'
